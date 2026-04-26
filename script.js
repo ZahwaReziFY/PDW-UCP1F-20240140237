0222ae8c9ef38923e4aa73ff16ee46bb3d20d8ab
ZahwaReziFY <zahwarezi416@gmail.com>
2026-04-26 23:33:51 +0700

    **menyeting ulang margin dan padding agar tampilan presisi, memilih font poppins, serta membuat background gradasi hitam dengan animasi bergerak agar web terlihat lebih aesthetic

---
+++ script.js
@@ -0,0 +1,46 @@
+// fade in
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add("fade-in");
+});
+
+// page transition
+document.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", function(e) {
+    let target = this.getAttribute("href");
+
+    if (target && !target.startsWith("#")) {
+      e.preventDefault();
+
+      document.body.classList.add("fade-out");
+
+      setTimeout(() => {
+        window.location.href = target;
+      }, 300);
+    }
+  });
+});
+
+// form
+if (document.getElementById("memberForm")) {
+  document.getElementById("memberForm").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let nama = document.getElementById("nama").value;
+    let email = document.getElementById("email").value;
+    let minat = document.getElementById("minat").value;
+
+    document.getElementById("result").innerHTML =
+      `<p>Berhasil: ${nama} - ${email} - ${minat}</p>`;
+
+    alert("Data berhasil disimpan!");
+  });
+}
+
+// audio
+function playAudio() {
+  document.getElementById("audio").play();
+}
+
+function pauseAudio() {
+  document.getElementById("audio").pause();
+}
