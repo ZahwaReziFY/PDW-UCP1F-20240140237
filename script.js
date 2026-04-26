63e2f8204687c784be61f78c7595b04d012645da
ZahwaReziFY <zahwarezi416@gmail.com>
2026-04-26 23:59:14 +0700

    mengatur perilaku semua tautan agar saat diklik, proses perpindahan halaman ditunda sejenak untuk menjalankan animasi transisi memudar (fade-out) terlebih dahulu sebelum akhirnya mengarahkan pengguna ke alamat tujuan

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 window.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("fade-in");
 });
+
+document.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", function(e) {
+    let target = this.getAttribute("href");
+
+    if (target && !target.startsWith("#")) {
+      e.preventDefault();
+
+      document.body.classList.add("fade-out");
+
+      setTimeout(() => {
+        window.location.href = target;
+      }, 300);
+    }
+  });
+});
