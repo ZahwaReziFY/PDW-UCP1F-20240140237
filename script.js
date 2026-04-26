e84d9404e4924f3b0b26376001be3ffd5b392181
ZahwaReziFY <zahwarezi416@gmail.com>
2026-04-26 23:59:44 +0700

    menambahkan fungsi penanganan formulir yang mencegah pemuatan ulang halaman saat dikirim, mengambil nilai input dari pengguna, lalu secara dinamis menampilkan konfirmasi data serta memunculkan pesan peringatan bahwa proses penyimpanan telah berhasil

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ document.querySelectorAll("a").forEach(link => {
     }
   });
 });
+
+if (document.getElementById("memberForm")) {
+  document.getElementById("memberForm").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let nama = document.getElementById("nama").value;
+    let email = document.getElementById("email").value;
+    let minat = document.getElementById("minat").value;
+
+    document.getElementById("result").innerHTML =
+      `<p>Berhasil: ${nama} - ${email} - ${minat}</p>`;
+
+    alert("Data berhasil disimpan!");
+  });
+}
